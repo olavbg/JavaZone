2b6083c2ce3edec73d9929b6696c9f2d6cfa987e
Olav Blaaflat <olgulbra@hotmail.com>
2026-09-08 19:07:56 +0200

    fix: resolve SessionDetail compile issue

--- app/src/main/java/com/olavbg/javazone/ui/detail/SessionDetailScreen.kt
+++ app/src/main/java/com/olavbg/javazone/ui/detail/SessionDetailScreen.kt
@@ -16,6 +16,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.core.net.toUri
