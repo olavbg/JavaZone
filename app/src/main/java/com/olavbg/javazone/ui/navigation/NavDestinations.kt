2270573f68d8aae789afd745708cece4ca565345
Olav Blaaflat <olgulbra@hotmail.com>
2026-09-08 19:00:57 +0200

    refactor: remove archive navigation destination

---
+++ app/src/main/java/com/olavbg/javazone/ui/navigation/NavDestinations.kt
@@ -0,0 +1,16 @@
+package com.olavbg.javazone.ui.navigation
+
+import androidx.navigation3.runtime.NavKey
+import kotlinx.serialization.Serializable
+
+@Serializable
+sealed interface NavDestination : NavKey {
+    @Serializable
+    data object Timeline : NavDestination
+
+    @Serializable
+    data class SessionDetail(val sessionId: String) : NavDestination
+
+    @Serializable
+    data object Settings : NavDestination
+}
